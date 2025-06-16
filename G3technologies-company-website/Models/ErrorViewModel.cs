aa1579b603e7c67d58d8629c3596fc473c2ada92
Chathushi Kavindya <chathushikavindya09@gmail.com>
2025-06-17 01:32:42 +0530

    Initial Commit

---
+++ G3technologies-company-website/Models/ErrorViewModel.cs
@@ -0,0 +1,9 @@
+namespace G3technologies_company_website.Models
+{
+    public class ErrorViewModel
+    {
+        public string? RequestId { get; set; }
+
+        public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
+    }
+}
